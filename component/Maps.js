609a56eaefca1711caddf50895c286981fc95c14
Rmjp <rmjp15.rmt@gmail.com>
2023-06-24 22:21:11 +0700

    add: Mapp

--- component/Maps.js
+++ component/Maps.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-import { Marker } from '@react-google-maps/api';
-import { HeatmapLayer } from '@react-google-maps/api';
+import {HeatmapLayerF, MarkerF} from '@react-google-maps/api';
 
 
 const data_latlng = [{
@@ -9,21 +8,39 @@ const data_latlng = [{
   lng: 100.498
 },
 {
-  lat: 13.820,
+  lat: 13.721,
   lng: 100.498
 },
 {
-  lat: 13.920,
+  lat: 13.722,
   lng: 100.498
 },
 {
-  lat: 13.720,
-  lng: 100.598
+  lat: 13.721,
+  lng: 100.499
 },
 {
   lat: 13.720,
-  lng: 100.698
+  lng: 100.499
 }];
+
+
+const gradient = [
+  "rgba(0, 255, 255, 0)",
+  "rgba(0, 255, 255, 1)",
+  "rgba(0, 191, 255, 1)",
+  "rgba(0, 127, 255, 1)",
+  "rgba(0, 63, 255, 1)",
+  "rgba(0, 0, 255, 1)",
+  "rgba(0, 0, 223, 1)",
+  "rgba(0, 0, 191, 1)",
+  "rgba(0, 0, 159, 1)",
+  "rgba(0, 0, 127, 1)",
+  "rgba(63, 0, 91, 1)",
+  "rgba(127, 0, 63, 1)",
+  "rgba(191, 0, 31, 1)",
+  "rgba(255, 0, 0, 1)",
+];
 
 const containerStyle = {
   height: '100%'
@@ -36,30 +53,50 @@ const center = {
 
 function MyComponent(props) {
   const { isLoaded } = useJsApiLoader({
+    libraries: ['visualization'],
     id: 'google-map-script',
     googleMapsApiKey: "AIzaSyBv-q3qRdY4WeAmR940r8LWmQjrz6b8RII",
-    libraries: ['visualization']
   })
 
   const [map, setMap] = React.useState(null)
 
   const onLoad = React.useCallback(function callback(map) {
-    
-
     setMap(map)
   }, [])
 
   const onUnmount = React.useCallback(function callback(map) {
     setMap(null)
   }, [])
-
-  let heatmap = [];
+  const [currentZoom, setCurrentZoom] = React.useState(18)
+  const [marker, setMarker] = React.useState(1);
+  function handleZoomChanged() {
+    handleClick(this.getZoom());
+    setCurrentZoom(this.getZoom());
+    if(this.getZoom() <= 13){
+      setMarker(0);
+      heatmap.setMap(map);
+    }
+    else{
+      setMarker(1);
+      heatmap?.setMap(null);
+    }
+  }
+  function handleClick() {
+    console.log(currentZoom);
+  }
+  const [heatmap, setHeatmap] = React.useState(null);
+  const onLoadHeatmap = React.useCallback(function callback(maps) {
+    setHeatmap(maps);
+    maps.setMap(null);
+    maps.set("gradient", gradient);
+  }, []);
 
   return isLoaded ? (
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={14}
+        zoom={currentZoom}
+        onZoomChanged={handleZoomChanged}
         onLoad={onLoad}
         onUnmount={onUnmount}
 
@@ -69,20 +106,17 @@ function MyComponent(props) {
       >
 
       {/* Marker */}
-      {data_latlng.map((item, index) => (
-        <Marker position={item} key={index}></Marker>
-      ))}
+      {(marker == 1) ? data_latlng.map((item, index) => (
+        <MarkerF position={item} key={index}></MarkerF>
+      )) : <></>
+      }
+      
 
       {/* Heatmap Layer */}
-      <HeatmapLayer
-        onLoad={
-          (heatmap) =>{ heatmap = data_latlng.map((item, index) => {return new google.maps.LatLng(item.lat, item.lng)} )}
-        } 
-        data={
-          heatmap
-        } 
-      />
-
+      <HeatmapLayerF onLoad={onLoadHeatmap} data={
+        data_latlng.map((item, index) => {return new google.maps.LatLng(item.lat, item.lng)} )
+      } />
+<></>
       </GoogleMap>
   ) : <></>
 }
