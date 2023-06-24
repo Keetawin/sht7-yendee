918b5e5ce24aba984bb9a57f2e1d77283d5f9b7e
Keetawin <khitawanlimxrun@gmail.com>
2023-06-24 14:41:15 +0700

    build: yendee

---
+++ pages/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBywvIdiu2jV0P0so9VYSSLeXEzzY67zBY",
+  authDomain: "yenn-390805.firebaseapp.com",
+  projectId: "yenn-390805",
+  storageBucket: "yenn-390805.appspot.com",
+  messagingSenderId: "664626340135",
+  appId: "1:664626340135:web:f2b15eaa516f0c95d64902",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
