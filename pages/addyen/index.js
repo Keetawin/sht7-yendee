bd440e2f1689a42df01cd7941fa627c904c5cf5d
Keetawin <khitawanlimxrun@gmail.com>
2023-06-24 14:56:20 +0700

    add:firebase

---
+++ pages/addyen/index.js
@@ -0,0 +1,6 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import Link from "next/link";
+export default function AddYen() {
+  return <main>Test</main>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import Link from "next/link";
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Link
+        href="/addyen"
+        className="flex justify-center items-center w-12 h-12 rounded-full bg-[#060047] text-white text-2xl font-bold shadow focus:outline-none hover:bg-[#E90064] transition-colors duration-300"
+      >
+        +
+      </Link>
+    </main>
+  );
 }
